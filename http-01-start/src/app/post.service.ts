ed5d535f1f8ee2c8d65b1246f0e0ed9f57c8c05e
Seweryn Drag <dragseweryn@zohomail.eu>
2024-03-02 22:13:33 +0100

    http requests end

--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -18,11 +23,15 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://angular-udemy-4be22-default-rtdb.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response', // zwraca cały boiekt z httpresponse ; domyślnie body
+        }
       )
       .subscribe(
         (responseData) => {
           console.log(responseData);
+          console.log(responseData.body);
         },
         (error) => {
           this.error.next(error.message);
@@ -31,9 +40,21 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('read', 'slow');
+
     return this.http
       .get<{ [key: string]: Post }>(
-        'https://angular-udemy-4be22-default-rtdb.firebaseio.com/posts.json'
+        'https://angular-udemy-4be22-default-rtdb.firebaseio.com/posts.json',
+        {
+          responseType: 'json',
+          headers: new HttpHeaders({
+            'Custom-Header': 'Hello',
+          }),
+          params: searchParams,
+          // params: new HttpParams().set('print', 'pretty'),
+        }
       )
       .pipe(
         map((responseData) => {
@@ -53,8 +74,25 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(
-      'https://angular-udemy-4be22-default-rtdb.firebaseio.com/posts.json'
-    );
+    return this.http
+      .delete(
+        'https://angular-udemy-4be22-default-rtdb.firebaseio.com/posts.json',
+        {
+          observe: 'events',
+          // responseType: 'text',
+        }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            console.log('sent');
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log('response');
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
