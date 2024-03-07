4d69af07ae22a6edf2ef713603998072cc7f87fb
Seweryn Drag <dragseweryn@zohomail.eu>
2024-03-07 20:54:50 +0100

    sending fetch recipes with auth token

--- course-app/src/app/header/header.component.ts
+++ course-app/src/app/header/header.component.ts
@@ -1,14 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
+  private userSubscription: Subscription;
+  isAuthenticated = false;
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+      console.log(!user);
+      console.log(!!user);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
---
+++ course-app/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template:
+    '<div class="loadingio-spinner-pulse-ejj4s2b6iiq"><div class="ldio-m1x6w29mhhp"><div></div><div></div><div></div></div></div>',
+  styleUrls: ['./loading-spinner.component.css'],
+})
+export class LoadingSpinnerComponent {}
